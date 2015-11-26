a799c4d6178269ba75db0a27ff5947ddaff800ad
Vasiliy Litvinov <kvas.omsk@gmail.com>
2015-11-27 00:41:00 +0600

    dayend

--- freq_calc.py
+++ freq_calc.py
@@ -3,9 +3,23 @@
 
 import os
 import codecs
+import operator
 
-# возвращает словарь, в котором ключём является строчка-n-грамма, значением -- количество n-грамм в text 
-def calc_ngram_counts(text, n):
+# adds d2 to d1
+def join_dicts(d1, d2, conflict_solver):
+    for k,v in d2.items():
+        if k in d1:
+            d1[k] = conflict_solver(d1[k], k)
+        else:
+            d1[k] = v
+    return d1
+
+def ngram_counts_to_freqs(count_ht, textlen):
+    normcoef = 1.0 / textlen
+    return { ng: (cnt * normcoef) for ng,cnt in count_ht.items() }
+
+# возвращает пару (словарь, в котором ключём является строчка-n-грамма, значением -- количество n-грамм в text ; аналогичный частотный словарь
+def calc_ngram_counts_freqs(text, n):
     count_ht = dict() 
     for i in xrange(len(text) - n + 1):
         ngram = text[i:i+n]
@@ -13,12 +27,7 @@ def calc_ngram_counts(text, n):
             count_ht[ngram] += 1
         else:
             count_ht[ngram] = 1
-    return count_ht
-
-# count_len_pair -- результат calc_ngram_counts
-def ngram_counts_to_freqs(count_ht, textlen):
-    normcoef = 1.0 / textlen
-    return { ng: (cnt * normcoef) for ng,cnt in count_ht.items() }
+    return (count_ht, ngram_counts_to_freqs(count_ht, len(text)))
 
 # разбиение текстов автора, тексты которого спрятаны в author_dir, на фрагменты размером frag_size символов 
 # (или меньше, в случае достижения конца файла)
@@ -38,27 +47,25 @@ def fragmentize_author(author_dir, frag_size):
 # возвращает словарь (строка -- имя автора; список фрагментов этого автора)
 def fragmentize_all_authors(teach_dir, frag_size):
     fragmentation = dict() 
+    tdir = os.path.join(teach_dir, '')
     for author_dir in os.listdir(teach_dir):
         author_dir = author_dir.strip('/')
-        fragmentation[author_dir] = fragmentize_author(author_dir, frag_size)
+        fragmentation[author_dir] = fragmentize_author(tdir + author_dir, frag_size)
     return fragmentation
 
-# good name; used only in author_ngrams_count
-def map_helper(fragment, ngram_len):
-    count_ht = calc_ngram_counts(fragment, ngram_len)
-    return (count_ht, ngram_counts_to_freqs(count_ht, len(fragment)))
-
 # возвращает кортеж (строка -- имя автора; (словарь с количествами слов; словарь с частотами слов))
-def author_ngrams_count(author_frag, n):
-    fragments = author_frag[1]
-    return (author_frag[0], map(calc_ngram_counts(lambda x : calc_ngram_counts(x, n))
+def author_ngrams_count(author_frag_pair, n):
+    fragments = author_frag_pair[1]
+    return (author_frag_pair[0], map(lambda x : calc_ngram_counts_freqs(x, n), fragments)) # (author_str, (count_ht, freq_ht))
 
-def count_all_authors(fragmentation):
+def count_all_authors(fragmentation, ngram_len):
+    return { author_dir : author_ngrams_count(author_frag_pair, ngram_len) for author_dir, author_frag_pair in fragmentation.items() }
 
-
-def get_top_k_ngrams(train_set, top_k):
-    
-
-
+def get_top_k_ngrams(author_ht_tuples, top_k):
+    total_count_dict = {}
+    for author_ht_tuple in author_ht_tuples:
+        join_dicts(total_count_dict, author_ht_tuple[1][0], operator.add)
+    sorted_dict = sorted(total_count_dict.items(), key = operator.itemgetter(1), reverse = True)
+    return map(operator.itemgetter(0), sorted_dict)[0:top_k]
 
 fragmentize_all_authors('filtertxt', 1500)
